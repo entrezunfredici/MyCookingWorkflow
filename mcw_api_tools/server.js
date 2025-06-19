da76772876b4f27f2d2b3810df69716732b59b35
entrezunfredici <superfred2468@gmail.com>
2025-06-19 08:44:09 +0200

    [IMP] server

--- mcw_api_tools/server.js
+++ mcw_api_tools/server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 
 db.instance.sync({ force: false }).then(async () => {
     console.log('\x1b[32m%s\x1b[0m', 'Database connected and synchronized');
-    app.listen(process.env.API_PORT, () => {
+    app.listen(process.env.API_PORT, '0.0.0.0',  () => {
         console.log('Server is running on:', `http://localhost:${process.env.API_PORT}`);
     })
 }).catch((e) => {
