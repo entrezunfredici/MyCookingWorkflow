9572f4658611a3c653f640689bb38a64a47b8187
entrezunfredici <superfred2468@gmail.com>
2025-06-19 09:42:08 +0200

    [ADD] proxy

--- mcw_api_users/app.js
+++ mcw_api_users/app.js
@@ -17,7 +17,7 @@ const UserDietRoutes = require("./routes/UserDiet.routes.js");
 const UserBlacklistedFoodRoutes = require("./routes/UserBlacklistedFood.routes.js");
 
 dotenv.config({ path: path.resolve(__dirname, ".env") }); // .env is placed in the root directory of the project
-
+const { setupProxies } = require('./proxy');
 const app = express();
 
 // CORS
@@ -29,24 +29,47 @@ app.use(
   })
 );
 
+// proxy
+setupProxies(app, [
+  {
+    url: "/mcw/tools",
+    proxy: {
+      target: `${process.env.VITE_API_TOOLS_URL}:${process.env.VITE_API_TOOLS_PORT}`,
+      changeOrigin: true,
+      pathRewrite: { "^/mcw/tools": "" },
+    },
+  },
+  {
+    url: "/mcw/food",
+    proxy: {
+      target: `${process.env.VITE_API_FOOD_URL}:${process.env.VITE_API_FOOD_PORT}`,
+      changeOrigin: true,
+      pathRewrite: { "^/mcw/food": "" },
+    },
+  },
+]);
+
 // Body parser
 app.use(bodyParser.json());
 
 // Middleware for favicon
 app.use(favicon(__dirname + "/public/favicon.ico"));
 
+const mcwRouter = express.Router();
+
 // Middleware pour servir la documentation Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+mcwRouter.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routes
-userRoutes(app);
-roleRoutes(app);
-dietRoutes(app);
-blacklistedFoodRoutes(app);
-UserDietRoutes(app);
-UserBlacklistedFoodRoutes(app);
+userRoutes(mcwRouter);
+roleRoutes(mcwRouter);
+dietRoutes(mcwRouter);
+blacklistedFoodRoutes(mcwRouter);
+UserDietRoutes(mcwRouter);
+UserBlacklistedFoodRoutes(mcwRouter);
 
 // ... Autres middlewares
+app.use("/mcw", mcwRouter);
 
 // Si rien n'est trouvé
 app.use(({ res }) => {
