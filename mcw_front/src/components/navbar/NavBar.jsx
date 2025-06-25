ec787865a882d8d941e5a44101391df57caf76a2
entrezunfredici <superfred2468@gmail.com>
2025-06-25 16:39:35 +0200

    [WIP] components

--- mcw_front/src/components/navbar/NavBar.jsx
+++ mcw_front/src/components/navbar/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'; // React is implicitly imported by bundlers like Vite/Next.js but it's good practice to keep it.
+import ThemeSwitcher from './ThemeSwitcher.jsx';
 
-function navbar() {
+const NavBar = () => {
   // Components return JSX (JavaScript XML), which looks like HTML
     return (
         <div>
             <h1>My Cooking Workflow!</h1>
+            <button id="switch_theme">light_mode</button>
             <button id="switch_to_main">main page</button>
+            <ThemeSwitcher />
         </div>
     );
 }
 
 // Don't forget to export your component so it can be used elsewhere!
-export default navbar;
+export default NavBar;
